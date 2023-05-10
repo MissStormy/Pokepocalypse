240e189dfd4eda5a73756b0f974f4db5112d17b6
LilithRelish <laragoblin@outlook.com>
2023-05-10 17:56:24 +0200

    El diseño grafico es mi pasion

--- src/main/java/com/pokejavafx/pokejavafx/AppMainCtrl.java
+++ src/main/java/com/pokejavafx/pokejavafx/AppMainCtrl.java
@@ -1,14 +1,47 @@
 package com.pokejavafx.pokejavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppMainCtrl {
     @FXML
-    private Label welcomeText;
+    private Button BtnCrear;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button BtnLogin;
+    @FXML
+    private TextField TxtCrearPass;
+
+    @FXML
+    private TextField TxtCrearUsuario;
+
+    @FXML
+    private TextField TxtPass;
+
+    @FXML
+    private TextField TxtUsuario;
+
+    @FXML
+    void OnClickCrear(ActionEvent event){
+
     }
+
+    @FXML
+    void OnClickLogin(ActionEvent event) throws IOException  {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle("PokeJavaFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
---
+++ src/main/java/com/pokejavafx/pokejavafx/HomeCtrl.java
@@ -0,0 +1,11 @@
+/**
+ * @author Lara Rodriguez
+ */
+package com.pokejavafx.pokejavafx;
+
+import java.util.*;
+
+public class HomeCtrl {
+    public static void main(String[] args) {
+    }
+}
