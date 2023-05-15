5aa05e5452b017dd3f5a41292ebceb6724649820
LilithRelish <laragoblin@outlook.com>
2023-05-15 12:40:39 +0200

    Loca con el clicker

--- src/main/java/com/pokejavafx/pokejavafx/ClickerCtrl.java
+++ src/main/java/com/pokejavafx/pokejavafx/ClickerCtrl.java
@@ -3,7 +3,72 @@
  */
 package com.pokejavafx.pokejavafx;
 
-public class ClickerCtrl {
-    public static void main(String[] args) {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClickerCtrl implements Initializable {
+    @FXML
+    private Label LbProgress;
+    @FXML
+    private Button BtnCaminar;
+
+    @FXML
+    private Button BtnClose;
+
+    @FXML
+    private Button BtnGoBack;
+
+    @FXML
+    private ProgressBar PgBar;
+    double progress = 0.0;
+    @FXML
+    void OnClickCamina(ActionEvent event) {
+        if(progress < 1){
+            progress += 0.1;
+            PgBar.setProgress(progress);
+            LbProgress.setText(Integer.toString((int)Math.round(progress*100)) + " Pasos dados");
+        }
+
+
+    }
+
+    @FXML
+    void OnClickClose(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void OnClickGoBack(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle("PokeJavaFX");
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+
+        Stage stagePrincipal = (Stage) BtnGoBack.getScene().getWindow();
+        stagePrincipal.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        PgBar.setStyle("-fx-accent: #00FF00;");
     }
 }
--- src/main/java/com/pokejavafx/pokejavafx/PokedexCtrl.java
+++ src/main/java/com/pokejavafx/pokejavafx/PokedexCtrl.java
@@ -5,12 +5,18 @@ package com.pokejavafx.pokejavafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.swing.text.html.ImageView;
+import java.io.IOException;
 import java.util.*;
 
 public class PokedexCtrl {
@@ -66,12 +72,23 @@ public class PokedexCtrl {
 
     @FXML
     void OnClickClose(ActionEvent event) {
-
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
-    void OnClickGoBack(ActionEvent event) {
+    void OnClickGoBack(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle("PokeJavaFX");
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
 
+        Stage stagePrincipal = (Stage) BtnGoBack.getScene().getWindow();
+        stagePrincipal.close();
     }
 
     @FXML
